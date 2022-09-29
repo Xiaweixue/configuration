46d604324b0ae2f24c0af430e61cf1911daccc36
“Xiaweixue” <2742377217@qq.com>
2022-09-29 19:48:12 +0800

    feat(custom): 代码规范配置完成

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,6 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <div>da6edqw55
-    </div>
   </div>
 </template>
 
